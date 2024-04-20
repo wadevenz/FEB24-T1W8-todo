cc4e40a76d124f26e9add55141e8bb3f39a0c492
[wadevenz] <[w.venz001@gmail.com]>
2024-04-20 13:11:45 +1000

    edit bash file

--- run.sh
+++ run.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
+# for assignment
+# check if python3 is available
+# check if venv exists
 
 python3 -m venv .venv
 source .venv/bin/activate
 pip3 install colored
 python3 main.py
+
+#for another bash file can add
+
+#chmod +x another.sh
+# ./another.sh
